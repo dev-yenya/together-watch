d8d80d639ffe4c4ca4c3a8ee14aebeea9aa5c32c
hyunvely8 <rlacodus131@gmail.com>
2023-11-22 14:16:30 +0900

    <type> : <기본 UI 완성> <body> floating button, navigation view, 설정 (로그아웃, 회원 탈퇴), 약속 create 일부 구현

--- app/src/main/java/com/example/together_watch/MainActivity.kt
+++ app/src/main/java/com/example/together_watch/MainActivity.kt
@@ -1,46 +1,57 @@
 package com.example.together_watch
-
 import android.os.Bundle
+import android.widget.CalendarView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.together_watch.ui.BottomBar
+import com.example.together_watch.ui.NavigationGraph
 import com.example.together_watch.ui.theme.Together_watchTheme
-
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Together_watchTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
     }
+
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen() {
+    val navController: NavHostController = rememberNavController()
+    val buttonsVisible = remember { mutableStateOf(true) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Together_watchTheme {
-        Greeting("Android")
+    Scaffold(
+        bottomBar = {
+            BottomBar(
+                navController = navController,
+                state = buttonsVisible,
+                modifier = Modifier
+            )
+        }) { paddingValues ->
+        Box(
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            NavigationGraph(navController = navController)
+        }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+
     }
+   
 }
 
 rootProject.name = "together_watch"
