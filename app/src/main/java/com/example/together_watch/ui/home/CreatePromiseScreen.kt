a474e8dbd808d005205978e13064405be23d911a
NamJihyun99 <namjihyun9936@gmail.com>
2023-11-30 02:16:41 +0900

    docs:> comment todo/#14

--- app/src/main/java/com/example/together_watch/ui/home/CreatePromiseScreen.kt
+++ app/src/main/java/com/example/together_watch/ui/home/CreatePromiseScreen.kt
@@ -42,7 +42,7 @@ fun CreatePromiseScreen() {
     val nextScreen = { currentScreen.intValue++ }
     val context = LocalContext.current
     val complete = {
-        shareInvitation(context, groupId)
+        shareInvitation(context)
         // TODO: 약속 생성시 아이디 전달
     }
 
