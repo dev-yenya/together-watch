1e04ad02e29d0516e709f28e5af2c81d950c5c19
dev-yenya <dev.yenya@gmail.com>
2023-12-02 23:43:10 +0900

    fix: Resolve "Unresolved reference: observeAsState" error
    Added the necessary dependencies for Jetpack Compose and LiveData to resolve the "Unresolved reference: observeAsState" error.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -114,4 +114,8 @@ dependencies {
 
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.5")
     implementation("androidx.navigation:navigation-compose:2.5.3")
+
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
+
+
 }
